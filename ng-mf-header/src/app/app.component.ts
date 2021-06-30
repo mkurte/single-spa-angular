a26a21a964dc1a8ded62b44e51c033f52c982fbb
Matthias Kurte <matthias.kurte@plan.one>
2021-06-30 22:01:14 +0200

    feat: add microfrontend app for header

---
+++ ng-mf-header/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mf-header-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-mf-header';
+}
